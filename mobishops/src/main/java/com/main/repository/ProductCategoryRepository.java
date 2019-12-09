6b8cac6fe51351e812054f8f93a72031f3edc27e
dinhdeveloper <dinhtrancntt@gmail.com>
2019-12-09 20:43:13 +0700

    ok first

--- mobishops/src/main/java/com/main/repository/ProductCategoryRepository.java
+++
@@ -1,9 +0,0 @@
-package com.main.repository;
-
-import com.main.model.ProductCategory;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ProductCategoryRepository extends JpaRepository<ProductCategory,Integer> {
-}
